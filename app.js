c170f16f0a15c6de9ed5e23d576905adf371b0fd
divyaelagandula <divyaelagandula9014@gmail.com>
2025-08-28 11:27:52 +0530

    fourth

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const app = express();
+const productroutes = require('./routes/products');
+const categorieroutes = require('./routes/categories');
+
 app.use((req, res, next) => {
   console.log(`${req.method} request made to ${req.url}`);
   next();
 });
-app.get("/products",(req,res)=>{
-    res.send(`<h1>Here is the list of all products</h1>`)
-})
-app.get("/categories",(req,res)=>{
-    res.send(`<h1>Here is the list of all categories</h1>`)
-})
-app.post("/products",(req,res)=>{
-    res.send(`<h1>A new product has been added.</h1>`)
-})
-app.post("/categories",(req,res)=>{
-    res.send(`<h1>A new category has been created</h1>`)
-})
+
+app.use('/products', productroutes);
+app.use('/categories', categorieroutes);
 
 app.listen(3000, () => {
-    console.log("server is running on port 3000");
+  console.log('server is running on port 3000');
 });
